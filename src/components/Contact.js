8e912940e69aa0d00af5f946fcc103ade9287384
DavidJoao <davidsandoval596@gmail.com>
2022-09-14 22:08:16 -0700

    updated contact phone numbers

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,8 +6,8 @@ function Contact(){
         <div className='mb-5' style={{height:'auto', width:"100%", display:'flex', flexDirection:'column', justifyContent:'center', alignContent:'center'}}>
             <h1 style={{color:'white', textAlign:'center'}} className="m-5">Contact Us</h1>
             <div className='mx-auto p-5 d-flex flex-column flex-md-row w-100'>
-                <Staff name={"Veronica Rivera"} phone={""} position={"Secretary"} />
-                <Staff name={"Byanka Arceo"} phone={""} position={"Secretary"} />
+                <Staff name={"Veronica Rivera"} phone={"+1 (661)-382-9051"} position={"Secretary"} />
+                <Staff name={"Byanka Arceo"} phone={"+1 (661)-618-0129 "} position={"Secretary"} />
             </div>
             <h3 style={{color:'white', textAlign:'center'}}>Or send us an email</h3>
             <div style={{width:'100%', border:'1px solid white', padding:'5%', width:'80%', margin:'0 auto', borderRadius:'10px', boxShadow:'-2px 2px 30px 0px rgba(255,255,255,0.75)'}} className="mt-5">
